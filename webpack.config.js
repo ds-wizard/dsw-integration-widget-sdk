1dbb1fcdbca597fd261e98051a68f5f2e52ed127
Jan Slifka <jan.slifka@gmail.com>
2021-11-09 16:38:25 +0100

    Set up GitHub CI

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = (env, argv) => {
 
     const conf = {
-        mode: 'development',
+        mode: 'production',
         devServer: {
             open: true,
             static: {
